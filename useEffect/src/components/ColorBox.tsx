cb6e29c8c2fe3124adae66286b7ed87e0b8bfbd3
Daniel Pires <dfpires@gmail.com>
2024-08-20 20:35:05 -0300

    Add files via upload

---
+++ useEffect/src/components/ColorBox.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+
+function ColorBox({ color }: { color: string }) {
+    const [bgColor, setBgColor] = useState<string>('white');
+
+    useEffect(() => {
+        setBgColor(color);
+    }, [color]);
+
+    return (
+        <div style={{backgroundColor: bgColor, width: '200px', 
+            height: '200px', border: '3px solid black', borderRadius: '50%'
+        }}>
+            <p style={{textAlign: 'center', lineHeight: '165px', color: 'black'}}> Cor Atual: {bgColor} </p>
+        </div>
+    );
+}
+
+export default ColorBox;
---
+++ useEffect/src/components/Message2.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+function Message() {
+    const [message, setMessage] = useState("Boa tarde");
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setMessage("Boa noite");
+        }, 10000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div>
+            <h3>{message}</h3>
+        </div>
+    );
+}
+
+export default Message;
