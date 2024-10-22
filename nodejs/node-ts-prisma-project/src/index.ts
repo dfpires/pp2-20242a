f510e40de41f358cf12787661f5e55a5c8708ad0
Daniel Pires <dfpires@gmail.com>
2024-10-21 21:16:07 -0300

    Add files via upload

--- nodejs/node-ts-prisma-project/src/index.ts
+++ nodejs/node-ts-prisma-project/src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';  // Importa o middleware cors
 import { routes } from './routes';
 
 const app = express();
 const port = 3000;
+
+// Habilitar CORS para permitir que a API seja acessada de domínios externos
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
